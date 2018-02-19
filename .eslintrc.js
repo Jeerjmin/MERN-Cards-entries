e86166bccbc33c8fd4083ec2869afaa35fd98cb3
Oleg <31519602+Jeerjmin@users.noreply.github.com>
2018-02-19 10:02:03 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,4 @@
 module.exports = {
-    // Среды - среды, в которых ваши .js файлы будут запущены. Каждая среда предоставляет определенный набор предопределенных глобальных переменных.
   'env': {
       'node': true,
       'es6': true
@@ -15,11 +14,10 @@ module.exports = {
     "plugins": [
         "react"
   ],
-    // Здесь таятся наши правила.
     'rules': {
-        'indent': [2, 4], // оповещать как ошибку
-        'brace-style': [2, '1tbs'], // оповещать как ошибку
-        'no-unused-vars': [1], // оповещать как предупреждение
+        'indent': [2, 4], 
+        'brace-style': [2, '1tbs'], 
+        'no-unused-vars': [1], 
         'no-console': [0],
         "react/jsx-uses-react": "error",
         "react/jsx-uses-vars": "error",
